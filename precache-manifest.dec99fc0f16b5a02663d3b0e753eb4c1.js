1aacf69f2bcd404ed572ce49468689d4ccafe8d8
shreyash prashu <shreyashprashu93@gmail.com>
2020-07-27 07:31:18 +0530

    Updates

---
+++ precache-manifest.dec99fc0f16b5a02663d3b0e753eb4c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9667098eadb2bedc2f88505d5e97bfc8",
+    "url": "/Covid19_World_Cases_Tracker/index.html"
+  },
+  {
+    "revision": "9a423559609f65afef39",
+    "url": "/Covid19_World_Cases_Tracker/static/css/main.120a81d9.chunk.css"
+  },
+  {
+    "revision": "84b6538bd6f843433063",
+    "url": "/Covid19_World_Cases_Tracker/static/js/2.468dd8bb.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19_World_Cases_Tracker/static/js/2.468dd8bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a423559609f65afef39",
+    "url": "/Covid19_World_Cases_Tracker/static/js/main.4134290a.chunk.js"
+  },
+  {
+    "revision": "a876bcf59cd764154669",
+    "url": "/Covid19_World_Cases_Tracker/static/js/runtime-main.959fb8f7.js"
+  },
+  {
+    "revision": "7e6da74622e5978ae23b41ad07ad071b",
+    "url": "/Covid19_World_Cases_Tracker/static/media/COVID-19.7e6da746.png"
+  }
+]);
